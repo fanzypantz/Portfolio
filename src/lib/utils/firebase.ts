651d2d60965f5a77076b3e48a16f273905085197
fanzypantz <dm@andreas-tollanes.com>
2022-06-23 21:13:01 +0200

    initial commit

---
+++ src/lib/utils/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	// ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export default db;
